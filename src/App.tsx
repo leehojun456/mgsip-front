7fe7481dc5e81b50b35c9bd53d24a60c086dc716
LEE HOJUN <hppmm@naver.com>
2024-04-12 22:28:24 +0900

    Design some layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
+import { RouterProvider } from "react-router-dom";
+import root from "./router/root";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App flex">
+      <div className='flex flex-col bg-blue-900 w-96 h-screen border-r gap-6'>
+          <a href='#' className='mx-6 text-white hover:bg-white hover:text-blue-900 h-14 flex flex-col justify-center rounded-md transition-all duration-300'>
+            Dashboard
+          </a>
+          <a href='#' className='mx-6 text-white hover:bg-white hover:text-blue-900 h-14 flex flex-col justify-center rounded-md transition-all duration-300'>
+            Users
+          </a>
+          <a href='#' className='mx-6 text-white hover:bg-white hover:text-blue-900 h-14 flex flex-col justify-center rounded-md transition-all duration-300'>
+            Dashboard
+          </a>
+          <a href='#' className='mx-6 text-white hover:bg-white hover:text-blue-900 h-14 flex flex-col justify-center rounded-md transition-all duration-300'>
+            Dashboard
+          </a>
+          <a href='#' className='mx-6 text-white hover:bg-white hover:text-blue-900 h-14 flex flex-col justify-center rounded-md transition-all duration-300'>
+            Dashboard
+          </a>
+      </div>
       
+      <header className="bg-white w-full border-b h-20">
+      <nav>dadsf</nav>
       </header>
+      <RouterProvider router={root} />
     </div>
   );
 }
---
+++ src/router/root.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Suspense, lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+
+const Loading = <div>Loading...</div>;
+const Main = lazy(() => import("../pages/MainPage"));
+
+const root = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={Loading}>
+        <Main />
+      </Suspense>
+    ),
+  },
+]);
+
+export default root;
